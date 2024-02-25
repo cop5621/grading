79b8d9b07bc4f7a7bb8913d0fcffb67533365090
Paul <paul@pgazz.com>
2024-02-25 12:28:35 -0500

    Grading script runner

---
+++ run.sh
@@ -0,0 +1,15 @@
+test_script=${1}
+compiler_project=${2}
+
+if [[ "${compiler_project}" == "" || "${test_script}" == "" ]]; then
+  echo "USAGE: $0 test_script compiler_project"
+  exit 1
+fi
+
+cd $compiler_project
+python3 -m venv .env/
+source .env/bin/activate
+grep $(echo $compiler_project | cut -f1 -d/) ../students.csv | cut -f2 -d, | tee ${test_script}.out
+bash ../${test_script} | tee -a ${test_script}.out
+deactivate
+cd ../
